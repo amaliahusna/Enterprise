7268f6a70d6e9cf27f338941b6d25634e84cdab1
Husna Amalia <amalia.husna.ft19@mail.umy.ac.id>
2022-02-22 14:31:51 +0700

    first commit

---
+++ src/test/java/ti/enterprise202/refreshing/RefreshingApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.enterprise202.refreshing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
